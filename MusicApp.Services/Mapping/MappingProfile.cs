aaa532af5b6dc0b0d79eefdd57438fec2d827a42
Alfredo Castillo <alfredocastillo@Alfredos-MacBook-Pro.local>
2019-11-29 00:35:44 -0600

    added mapping profile to services

---
+++ MusicApp.Services/Mapping/MappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AutoMapper;
+using MusicApp.Data.Domain;
+using MusicApp.Services.Models;
+
+namespace MusicApp.Services.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Model
+            CreateMap<Album, AlbumModel>();
+            CreateMap<ArtistGenre, ArtistGenreModel>();
+            CreateMap<Artist, ArtistModel>();
+            CreateMap<Genre, GenreModel>();
+            CreateMap<Song, SongModel>();
+
+            // Model to Domain
+            CreateMap<AlbumModel, Album>();
+            CreateMap<ArtistGenreModel, ArtistGenre>();
+            CreateMap<ArtistModel, Artist>();
+            CreateMap<GenreModel, Genre>();
+            CreateMap<SongModel, Song>();
+        }
+    }
+}
