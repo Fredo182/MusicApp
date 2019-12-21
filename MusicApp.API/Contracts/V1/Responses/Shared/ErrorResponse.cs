39d6c60521a924e11e4d1fe4536e91b694df1b7f
Alfredo Castillo <alfredocastillo@Alfredos-MacBook-Pro.local>
2019-12-21 17:09:57 -0600

    added more errors

--- MusicApp.API/Contracts/V1/Responses/Shared/ErrorResponse.cs
+++ MusicApp.API/Contracts/V1/Responses/Shared/ErrorResponse.cs
@@ -12,6 +12,43 @@ namespace MusicApp.API.Contracts.V1.Responses.Shared
             Errors.Add(error);
         }
 
+        public ErrorResponse(string message)
+        {
+            Errors.Add(new ErrorModel { Message = message });
+        }
+
+        public ErrorResponse(string field, string message)
+        {
+            Errors.Add(new ErrorModel { FieldName = field, Message = message });
+        }
+
+        public ErrorResponse(string field, string message, IEnumerable<string> dynamicMessage)
+        {
+            foreach (var m in dynamicMessage)
+                message += Environment.NewLine + m;
+
+            ErrorModel error = new ErrorModel
+            {
+                FieldName = field,
+                Message = message
+            };
+
+            Errors.Add(error);
+        }
+
+        public ErrorResponse(string message, IEnumerable<string> dynamicMessage)
+        {
+            foreach (var m in dynamicMessage)
+                message += Environment.NewLine + m;
+
+            ErrorModel error = new ErrorModel
+            {
+                Message = message
+            };
+
+            Errors.Add(error);
+        }
+
         public List<ErrorModel> Errors { get; set; } = new List<ErrorModel>();
     }
 }
